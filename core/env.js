104968a6077387901495fcc88edd30d368f0a70d
Harsha-E <harshaedupuganti70@gmail.com>
2026-06-01 19:59:19 +0530

    fix: unignore core/env.js for deployment

---
+++ core/env.js
@@ -0,0 +1,14 @@
+/**
+ * MedCare | Environment Config
+ * Do NOT commit this file to public repositories.
+ */
+
+export const ENV = {
+  FIREBASE_API_KEY: "AIzaSyDfF54qdvqxaasAPLqhx2axoSASLQSvkN4",
+  FIREBASE_AUTH_DOMAIN: "cp-v1-ca134.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "cp-v1-ca134",
+  FIREBASE_STORAGE_BUCKET: "cp-v1-ca134.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "864742646610",
+  FIREBASE_APP_ID: "1:864742646610:web:642ced3c456700e876108b",
+  APP_VERSION: "v0.9.1"
+};
